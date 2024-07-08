61626012bc160d13214aeb9072745becc0e5db1d
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-07-08 09:45:37 +0200

    added click event

--- TS-TIC-TAC-TOE/src/components/Game.tsx
+++ TS-TIC-TAC-TOE/src/components/Game.tsx
@@ -9,25 +9,32 @@ export const Game = () => {
   type Cell = Player | null;
   type Board = Cell[];
 
+  const handleClick = (index: number) => {
+    if (board[index] === null) {
+      const newBoard = board.slice();
+      newBoard[index] = currentPlayer;
+      setBoard(newBoard);
+      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+    }
+  };
+  const renderCell = (index: number) => (
+    <div className="cell" onClick={() => handleClick(index)}>
+      {board[index]}
+    </div>
+  );
+
+  const renderRow = (rowIndex: number) => (
+    <div className="row">
+      {Array(3)
+        .fill(null)
+        .map((_, cellIndex) => renderCell(rowIndex * 3 + cellIndex))}
+    </div>
+  );
   return (
-    <>
-      <div className="game-board">
-        <div className="row">
-          <div className="cell" id="cell-0"></div>
-          <div className="cell" id="cell-1"></div>
-          <div className="cell" id="cell-2"></div>
-        </div>
-        <div className="row">
-          <div className="cell" id="cell-3"></div>
-          <div className="cell" id="cell-4"></div>
-          <div className="cell" id="cell-5"></div>
-        </div>
-        <div className="row">
-          <div className="cell" id="cell-6"></div>
-          <div className="cell" id="cell-7"></div>
-          <div className="cell" id="cell-8"></div>
-        </div>
-      </div>
-    </>
+    <div className="game-board">
+      {Array(3)
+        .fill(null)
+        .map((_, rowIndex) => renderRow(rowIndex))}
+    </div>
   );
 };
